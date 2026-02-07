6d581f4103f049e4cd608ac5af08d63a38a47de9
Md. Saifur Rahman <saifur@erainfotechbd.com>
2026-02-07 22:43:12 +0600

    Update

--- routes/api.php
+++ routes/api.php
@@ -54,7 +54,7 @@ Route::get('gold-price-status', [GoldFrontEnd::class, 'goldStatus']); //UAE Dirh
 Route::get('gold-price-business-insider', [GoldFrontEnd::class, 'goldPriceBI']); //UAE Dirham price
 Route::get('gold-price-apon-jewelary', [GoldFrontEnd::class, 'showBroadcastData']); //UAE Dirham price
 Route::get('gold-price-crystal-jewelary', [GoldFrontEnd::class, 'showBroadcastDataCrystal']); //UAE Dirham price
-Route::get('crystal-gold', [BroadcastStreamControllerStream::class, 'stream']);
+Route::get('crystal-gold', [BroadcastControllerStream::class, 'stream']);
 
 Route::get('site-setting', [SettingController::class, 'index']); //app setting
 
